e51ff6b7da133c42b885548538af11bb81f7dc91
Rohit Singh <27754873+razein97@users.noreply.github.com>
2023-03-06 21:46:35 +0530

    idk

---
+++ rent_calculator_svelte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class=" flex items-center justify-center h-screen">
+	<div class="flex flex-col items-center">
+		<p>Welcome to Rent Calculator</p>
+		<br />
+		<p>Log in with your account to continue</p>
+		<br />
+		<button class="bg-amber-500  hover:bg-amber-700 text-white font-bold py-2 px-4 rounded">
+			Login
+		</button>
+	</div>
+</div>

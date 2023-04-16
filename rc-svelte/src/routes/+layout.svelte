5f59e83d69b06a2282e0bddbe19896011d6f64af
Rohit Singh <27754873+razein97@users.noreply.github.com>
2023-04-16 20:47:23 +0530

    some stuff

---
+++ rc-svelte/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import '@fontsource/lato';
+	import '@fontsource/open-sans';
+	import '@fontsource/roboto';
+	import '@fontsource/inter';
+</script>
+
+<div class="min-h-screen w-full">
+	<slot />
+</div>

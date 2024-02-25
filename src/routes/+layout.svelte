cdd25b34ab335ced1b4f9f344d934502216e4af0
Rohit Singh <27754873+razein97@users.noreply.github.com>
2024-02-25 16:38:26 +0530

    some

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+	import '../app.pcss';
 	import type { LayoutData } from './$types';
+	import { Toaster } from 'svelte-sonner';
+	import { page } from '$app/stores';
+	import { beforeNavigate } from '$app/navigation';
+	import { getFlash } from 'sveltekit-flash-message/client';
 
 	export let data: LayoutData;
 </script>
 
-<slot />
+<Toaster richColors closeButton />
+
+<div class="grid">
+	<Navbar />
+	<!-- <DrawerMenu {showUserMenu} /> -->
+
+	<div class="h-screen min-h-screen !scroll-smooth lg:ml-72">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,19 @@
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 	import Database from 'tauri-plugin-sql-api';
+	import Icon from '@iconify/svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	export let data: PageData;
 
-	onMount(async () => {
-		const db = await Database.load('sqlite:rc.db');
-	});
+	let loading = true;
+
+	onMount(async () => {});
 </script>
 
-<p>hello</p>
+<div class="flex h-screen w-full p-2">
+	<Button href="/buildings" class="flex h-full w-full flex-col border">
+		<Icon class="mx-auto h-10 w-10" icon="fluent-emoji-flat:office-building" />
+		<p>Go to buildings</p>
+	</Button>
+</div>
